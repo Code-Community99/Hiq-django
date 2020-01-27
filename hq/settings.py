e5612f87b6693a77ed6e9c11d723d8afc25759f2
PaymasterMax <duncansantiago18@gmail.com>
2020-01-27 11:31:36 -0700

    message

--- hq/settings.py
+++ hq/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kt_*yi1px_k5%z^)#5kn&0ste+are41_rl_eia6o&y+nu9d*=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
