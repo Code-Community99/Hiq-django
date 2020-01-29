903821ffa2c93e4e3863074bdc7957fcd895ba31
PaymasterMax <duncansantiago18@gmail.com>
2020-01-29 11:50:23 -0700

    Bug fix

--- hq/settings.py
+++ hq/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'login',
     'events',
