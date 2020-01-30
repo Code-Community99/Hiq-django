dfb10a1192ecfd1a0d9d94e5d4707349e31f9f9b
PaymasterMax <duncansantiago18@gmail.com>
2020-01-30 08:14:46 -0700

    update

--- home/views.py
+++ home/views.py
@@ -3,9 +3,18 @@ from django.http import HttpResponse
 from .forms import loginfrm
 from events.models import events_list
 import datetime
+from signup.models import signup_user
+from django.contrib.auth.hashers import make_password
+
 
 # Create your views here.
 def home(request):
+    passws = signup_user.objects.all()
+
+    for x in passws:
+        x.Password = make_password(x.Password)
+        x.save()
+        print("\n\n\n\n\n\n\n{}".format(x))
     try:
         events_list.objects.filter(eventup_date__lte = datetime.datetime.now()).delete()
     except Exception as e:
@@ -13,7 +22,7 @@ def home(request):
     else:
         pass
 
-        
+
     form = loginfrm()
     if "username" in request.session:
         lcontrol = True
