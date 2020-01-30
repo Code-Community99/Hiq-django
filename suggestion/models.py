02902ae2c221d4bab4146826dbde6c2883294ff3
PaymasterMax <duncansantiago18@gmail.com>
2020-01-30 05:57:10 -0700

    upgrade db

--- suggestion/models.py
+++ suggestion/models.py
@@ -6,7 +6,7 @@ from signup.models import signup_user
 class suggetionbox(models.Model):
     sid = models.AutoField(primary_key = True)
 
-    sugguser = models.ForeignKey(signup_user , max_length = 50 ,default = 1 , on_delete = models.CASCADE)
+    sugguser = models.ForeignKey(signup_user , max_length = 50, on_delete = models.CASCADE)
 
     suggcontent = models.TextField(max_length = 255)
 
