3cf53c7b10fdec1f8bd364155e3faca704c7fb99
PaymasterMax <duncansantiago18@gmail.com>
2020-01-30 06:30:35 -0700

    Bug fix

--- suggestion/models.py
+++ suggestion/models.py
@@ -6,9 +6,9 @@ from signup.models import signup_user
 class suggetionbox(models.Model):
     sid = models.AutoField(primary_key = True)
 
-    sugguser = models.ForeignKey(signup_user , max_length = 50, on_delete = models.CASCADE)
+    sugguser = models.ForeignKey(signup_user , default = 1, max_length = 50, on_delete = models.CASCADE)
 
-    suggcontent = models.TextField(max_length = 255)
+    suggcontent = models.CharField(max_length = 255)
 
 
 
